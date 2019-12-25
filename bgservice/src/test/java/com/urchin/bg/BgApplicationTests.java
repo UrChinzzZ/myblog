f63c994c57687fe2f3bc5267d912a70b1c680eb5
UrChinzzZ <648453059@qq.com>
2019-12-25 15:10:53 +0800

    测试

---
+++ bgservice/src/test/java/com/urchin/bg/BgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urchin.bg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
