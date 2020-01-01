20d2578305812324520038e28462adfecb0f2555
urchin <648453059@qq.com>
2020-01-01 23:26:21 +0800

    更新博客主页  新添加归档页面，和标签页面

--- bgservice/src/main/resources/static/js/blog.js
+++ bgservice/src/main/resources/static/js/blog.js
@@ -2,9 +2,3 @@ $('.menu.toggle').click(function(){
     // 点击.menu.toggle 拿到所有的导航栏值
     $('.m-item').toggleClass('m-moblie-hide');
 })
-
-$('#payButton').popup({
-    popup:$('.payQR.popup'),
-    on:'click',
-    position:'bottom center'
-})
